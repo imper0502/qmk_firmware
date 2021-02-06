b6e9febfd90e71cb45c9b25bfd3327bdbeb60d31
imper0502 <yoj845@gmail.com>
2021-02-06 15:37:06 +0800

    🔧調整佈局

--- keyboards/atreus62/keymaps/default/keymap.c
+++ keyboards/atreus62/keymaps/default/keymap.c
@@ -14,38 +14,26 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BS] = LAYOUT( /* [> QWERTY <] */
             KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,
-    KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_PGUP,
+    KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_ENT,
     KC_ESC, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,
     KC_PSCR,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,                   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_PGDN,
-            KC_ESC, KC_TAB, KC_LGUI,KC_LSFT,KC_BSPC,KC_LCTL,KC_LALT,KC_SPC, MO(_DG),KC_MINS,KC_EQL, KC_ENT,
-                                                    KC_LCTL,KC_LALT
+            KC_ESC, KC_TAB, KC_LGUI,MO(_DG),KC_BSPC,KC_LCTL,KC_LALT,KC_SPC, MO(_DG),KC_MINS,KC_EQL, KC_ENT,
+                                                    KC_LGUI,KC_LSFT
   ),
-  /*
-   *    !       @     up     {    }        ||     pgup    7     8     9    *
-   *    #     left   down  right  $        ||     pgdn    4     5     6    +
-   *    [       ]      (     )    &        ||       `     1     2     3    \
-   *    FN2   insert super shift bksp ctrl || alt space   fn    .     0    =
-   */
   [_DG] = LAYOUT( /* [> RAISE <] */
-            _______,_______,_______,_______,_______,                _______,_______,_______,_______,_______,
-    _______,KC_EXLM,KC_AT,  KC_UP,  KC_LCBR,KC_RCBR,                KC_PGUP,KC_7,   KC_8,   KC_9,   KC_ASTR,_______,
-    _______,KC_HASH,KC_LEFT,KC_DOWN,KC_RGHT,KC_DLR,                 KC_PGDN,KC_4,   KC_5,   KC_6,   KC_PLUS,_______,
-    _______,KC_LBRC,KC_RBRC,KC_LPRN,KC_RPRN,KC_AMPR,                KC_GRV, KC_1,   KC_2,   KC_3,   KC_BSLS,_______,
-            TG(_FN),KC_INS, KC_LGUI,KC_LSFT,KC_BSPC,KC_LCTL,KC_LALT,KC_SPC, _______,KC_DOT, KC_0,   KC_EQL,
+            XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                XXXXXXX,XXXXXXX,KC_PSLS,KC_PAST,KC_BSPC,
+    XXXXXXX,KC_EXLM,KC_AT,  KC_UP,  KC_LCBR,KC_RCBR,                KC_PGUP,KC_7,   KC_8,   KC_9,   KC_PMNS,XXXXXXX,
+    TG(_FN),KC_HASH,KC_LEFT,KC_DOWN,KC_RGHT,KC_DLR,                 KC_PGDN,KC_4,   KC_5,   KC_6,   KC_PPLS,TG(_FN),
+    XXXXXXX,KC_LBRC,KC_RBRC,KC_LPRN,KC_RPRN,KC_AMPR,                KC_GRV, KC_1,   KC_2,   KC_3,   KC_PEQL,XXXXXXX,
+            KC_PSCR,KC_INS, KC_LGUI,_______,_______,_______,_______,_______,KC_0,   KC_COMM,KC_DOT, KC_EQL,
                                                     _______,_______
   ),
-  /*
-   *    insert home   up  end   pgup       ||      up     F7    F8    F9   F10
-   *     del   left  down right pgdn       ||     down    F4    F5    F6   F11
-   *          volup             reset      ||             F1    F2    F3   F12
-   *          voldn  super shift bksp ctrl || alt space   L0  prtsc scroll pause
-   */
   [_FN] = LAYOUT(
-            _______,_______,_______,_______,_______,                _______,KC_F10, KC_F11, KC_F12, _______,
-    _______,KC_INS, KC_HOME,KC_UP,  KC_END, KC_PGUP,                KC_UP,  KC_F7,  KC_F8,  KC_F9,  _______,_______,
-    _______,KC_DEL, KC_LEFT,KC_DOWN,KC_RGHT,KC_PGDN,                KC_DOWN,KC_F4,  KC_F5,  KC_F6,  _______,_______,
-    _______,KC_NO,  KC_VOLU,KC_NO,  KC_NO,  RESET,                  KC_NO,  KC_F1,  KC_F2,  KC_F3,  _______,_______,
-            KC_NO,  KC_VOLD,KC_LGUI,KC_LSFT,KC_BSPC,KC_LCTL,KC_LALT,KC_SPC, TO(_BS),KC_PSCR,KC_SLCK,KC_PAUS,
+            XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                XXXXXXX,KC_F10, KC_F11, KC_F12, XXXXXXX,
+    XXXXXXX,KC_INS, KC_HOME,KC_UP,  KC_END, KC_PGUP,                _______,KC_F7,  KC_F8,  KC_F9,  XXXXXXX,XXXXXXX,
+    XXXXXXX,KC_DEL, KC_LEFT,KC_DOWN,KC_RGHT,KC_PGDN,                _______,KC_F4,  KC_F5,  KC_F6,  XXXXXXX,XXXXXXX,
+    XXXXXXX,XXXXXXX,KC_VOLU,XXXXXXX,XXXXXXX,  RESET,                XXXXXXX,KC_F1,  KC_F2,  KC_F3,  XXXXXXX,XXXXXXX,
+            XXXXXXX,KC_VOLD,_______,TO(_BS),_______,_______,_______,_______,TO(_BS),KC_PSCR,KC_SLCK,KC_PAUS,
                                                     _______,_______
   )
 };
